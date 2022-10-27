dd99e6af561807407e65c20964858d9094bed114
Jono Tassia <jonotassia@gmail.com>
2022-10-27 16:15:15 +1100

    Major change to website front-end

---
+++ web_server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route(f'/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def web_page(page_name):
+    return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'Form submitted'
