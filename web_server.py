f583ea4b0202d4f5f8a8637bd5113ab4858f3651
Jono Tassia <jonotassia@gmail.com>
2022-10-28 12:05:49 +1100

    updated form to save to database

--- web_server.py
+++ web_server.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, session, flash
+import csv
 
 app = Flask(__name__)
+app.secret_key = '1feawrf233rdf@#%#R@F12131dwqada'
+
+
+def write_to_csv(data):
+    with open('./database.csv', 'a', newline="") as database:
+        name = data['name']
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+
+        file_writer = csv.writer(database)
+        file_writer.writerow([name, email, subject, message])
 
 
 @app.route(f'/')
@@ -13,6 +25,24 @@ def index():
 def web_page(page_name):
     return render_template(page_name)
 
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    return 'Form submitted'
+    if request.method == 'POST':
+        try:
+            # session['name'] = request.form['name']
+            response = request.form.to_dict()
+            write_to_csv(response)
+        except:
+            return 'Did not save to database'
+
+        flash(f"Thank you, {request.form['name']}. I will be in touch shortly.")
+        return redirect('./thank_you.html')
+
+    else:
+        return 'Something went wrong, try again'
+
+
+# @app.route('/thank_you.html')
+# def thank_you():
+#     return render_template('/thank_you.html', name=session['name'])
